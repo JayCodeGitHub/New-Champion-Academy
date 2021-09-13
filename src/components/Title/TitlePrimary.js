60a28cba74d252a49ec6bb9e4a3df48a97b8df32
Jakub Gryzinski <jakubgryzinski@MacBook-Pro-Jakub.local>
2021-09-13 11:15:53 +0200

    title and title primary center on mobile

--- src/components/Title/TitlePrimary.js
+++ src/components/Title/TitlePrimary.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 
 const Title = ({ title }) => (
   <>
-    <div className=" w-full flex lg:justify-center pl-24 lg:pl-0 items-center text-primary py-9 lg:text-3xl text-2xl">
+    <div className=" w-full flex justify-center  lg:pl-0 items-center text-primary py-9 lg:text-3xl text-2xl">
       {title}
     </div>
   </>
