fc4a9b42b31d3e3f26c9297d31cd4a1f712ca18b
Jakub <jakub.gryzinski@gmail.com>
2021-10-24 16:20:35 +0200

    change contact page layout

--- src/Layouts/ContactPageTemplate.js
+++ src/Layouts/ContactPageTemplate.js
@@ -7,41 +7,38 @@ import { AiFillYoutube } from "react-icons/ai"
 
 const ContactPageLayout = () => (
   <>
-    <div className="w-full pt-16">
-      <div>
-        <div className="h-32 flex justify-center items-center">
-          <div className="lg:w-1/3 w-3/4 h-full flex items-center justify-center text-white">
-            <div className="rounded-full h-14 w-14 mr-3 text-primary border-primary">
-              <LocationMarkerIcon />
-            </div>
-            Gołaszyńska 35, Oborniki
+    <div className="md:h-96 h-72 w-3/4 md:flex m-auto md:mt-32 mt-16">
+      <div className="md:h-96 h-72 md:w-1/2 w-full md:border-r-4 md:border-b-0 border-r-0 border-b-4">
+        <div className="h-1/3 w-full flex justify-center items-center">
+          <div className="rounded-full h-14 w-14 mr-3 text-primary border-primary">
+            <LocationMarkerIcon />
           </div>
+          Gołaszyńska 35, Oborniki
         </div>
-        <div className="h-32 flex justify-center items-center">
-          <div className="lg:w-1/3 w-3/4 h-full flex items-center justify-center text-white">
-            <div className="rounded-full h-14 w-14 mr-3  text-primary border-primary">
-              <MailIcon />
-            </div>
-            blochacademy@gmail.com
+        <div className="h-1/3 w-full flex justify-center items-center">
+          <div className="rounded-full h-14 w-14 mr-3  text-primary border-primary">
+            <MailIcon />
           </div>
+          blochacademy@gmail.com
         </div>
-        <div className="h-32 flex justify-center items-center">
-          <div className="lg:w-1/3 w-3/4 h-full flex items-center justify-center text-white">
-            <div className="rounded-full h-14 w-14 mr-3 text-primary border-primary">
-              <PhoneIcon />
-            </div>
-            696 162 080
+        <div className="h-1/3 w-full flex justify-center items-center">
+          <div className="rounded-full h-14 w-14 mr-3 text-primary border-primary">
+            <PhoneIcon />
           </div>
+          696 162 080
         </div>
       </div>
-      <div className="lg:w-1/3 w-3/4 h-96 m-auto flex justify-between items-center">
+      <div className="md:h-96 h-72 md:w-1/2 w-full">
         <a
           href="https://www.facebook.com/BlochChampionAcademy"
           target="_blank"
           rel="noreferrer"
         >
-          <div className="border-2 text-3xl rounded-full h-14 w-14 flex items-center justify-center text-primary border-primary hover:text-secondary hover:border-secondary">
-            <FaFacebookF />
+          <div className="h-1/3 w-full flex justify-center items-center">
+            <div className="border-2 text-3xl rounded-full h-14 w-14 flex items-center justify-center text-primary border-primary hover:text-secondary hover:border-secondary mr-7">
+              <FaFacebookF />
+            </div>
+            Facebook
           </div>
         </a>
         <a
@@ -49,8 +46,11 @@ const ContactPageLayout = () => (
           target="_blank"
           rel="noreferrer"
         >
-          <div className="border-2 text-3xl rounded-full h-14 w-14 flex items-center justify-center text-primary border-primary hover:text-secondary hover:border-secondary">
-            <FiInstagram />
+          <div className="h-1/3 w-full flex justify-center items-center">
+            <div className="border-2 text-3xl rounded-full h-14 w-14 flex items-center justify-center text-primary border-primary hover:text-secondary hover:border-secondary mr-7">
+              <FiInstagram />
+            </div>
+            Instagram
           </div>
         </a>
         <a
@@ -58,8 +58,11 @@ const ContactPageLayout = () => (
           target="_blank"
           rel="noreferrer"
         >
-          <div className="border-2 text-3xl rounded-full h-14 w-14 flex items-center justify-center text-primary border-primary hover:text-secondary hover:border-secondary">
-            <AiFillYoutube />
+          <div className="h-1/3 w-full flex justify-center items-center">
+            <div className="border-2 text-3xl rounded-full h-14 w-14 flex items-center justify-center text-primary border-primary hover:text-secondary hover:border-secondary mr-7">
+              <AiFillYoutube />
+            </div>
+            YouTube
           </div>
         </a>
       </div>
